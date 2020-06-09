a713d307c1f1a3a3a97a0ca169473d01e1d45a33
zshenf190882 <1908827735@qq.com>
2020-06-09 09:28:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/work/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.example.work.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.example.work.data.LoginDataSource;
+import com.example.work.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
